5bd7efc0d72cfd090be826e64ae5f7d4735099c9
REM-Project(PBL2021) <remprojectpbl@gmail.com>
2022-02-17 16:01:31 +0900

    IPアドレス設定の修正

--- demo/script/OpenPoseIpCamToJson.sh
+++ demo/script/OpenPoseIpCamToJson.sh
@@ -4,8 +4,8 @@ while true
 do
     echo "exec openpose $1"
     rm -f "$2/*" 
-    bin/OpenPoseDemo.exe --render_pose 0 --display 0 --disable_multi_thread --ip_camera $1 --write_json $2 --num_gpu 1 &
-    #bin/OpenPoseDemo.exe --disable_multi_thread --ip_camera $1 --write_json $2 --num_gpu 1 &
+    #bin/OpenPoseDemo.exe --render_pose 0 --display 0 --disable_multi_thread --ip_camera $1 --write_json $2 --num_gpu 1 &
+    bin/OpenPoseDemo.exe --disable_multi_thread --ip_camera $1 --write_json $2 --num_gpu 1 &
     wait
     echo "disconnect $1"
     sleep 3
